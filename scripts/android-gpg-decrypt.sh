ce1d43308909cb3c2c79a6684d1507972ebf26cc
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-09-20 22:57:38 +0200

    Add android release workflow

---
+++ scripts/android-gpg-decrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+--output ./android/app/keystore.jks ./android/app/keystore.jks.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+--output ./android/app/private_key.pepk ./android/app/private_key.pepk.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+--output ./android/app/google-private-key.json ./android/app/google-private-key.json.gpg
